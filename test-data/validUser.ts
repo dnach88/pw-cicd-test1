f51ad4c8cd3ea208e1cf7246111b8582222c2b73
dnach88 <mitkonachev88@gmail.com>
2025-09-08 12:36:04 +0300

    Update username

--- test-data/validUser.ts
+++ test-data/validUser.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 
 export const validUser = {
-  username: process.env.TEST_USER ?? 'defaulttUser',
+  username: process.env.TEST_USER ?? 'defaultUser',
   password: process.env.TEST_PASS ?? 'defaulttPass',
 };
